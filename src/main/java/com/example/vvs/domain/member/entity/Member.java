c53b326482c6c4cb4005296b124035e8e6168af1
HyeGwang Kang <kingaser@naver.com>
2023-10-10 17:00:44 +0900

    feat : 회원 엔티티 생성

--- src/main/java/com/example/vvs/domain/member/entity/Member.java
+++ src/main/java/com/example/vvs/domain/member/entity/Member.java
@@ -1,4 +1,47 @@
 package com.example.vvs.domain.member.entity;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@NoArgsConstructor(access = PROTECTED)
+@Getter
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 10, nullable = false)
+    private String userName;
+    @Column(length = 10, nullable = false)
+    private String birth;
+    @Column(length = 50, nullable = false)
+    private String email;
+    @Column(length = 50, nullable = false)
+    private String address;
+    @Column(length = 2, nullable = false)
+    private String gender;
+    @Column(length = 13, nullable = false)
+    private String phoneNumber;
+    @Column(length = 8, nullable = false)
+    private String role;
+
+    @Builder
+    public Member(Long id, String userName, String birth, String email,
+                  String address, String gender, String phoneNumber, String role) {
+        this.id = id;
+        this.userName = userName;
+        this.birth = birth;
+        this.email = email;
+        this.address = address;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+    }
 }
