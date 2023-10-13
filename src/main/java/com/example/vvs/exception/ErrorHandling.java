0a503046ab489282a0a462a216b3d00e44e1cef1
huinnii <huin32@gmail.com>
2023-10-13 14:36:04 +0900

    feat : 상품 삭제시 검색 예외 처리 추가

--- src/main/java/com/example/vvs/exception/ErrorHandling.java
+++ src/main/java/com/example/vvs/exception/ErrorHandling.java
@@ -10,7 +10,8 @@ public enum ErrorHandling {
 
     NO_UNIQUE_EMAIL(HttpStatus.BAD_REQUEST, "입력하신 이메일은 이미 존재하는 이메일입니다"),
     NO_UNIQUE_PHONENUMBER(HttpStatus.BAD_REQUEST, "입력하신 폰번호는 이미 존재하는 번호입니다"),
-    IS_EMPTY_LIST(HttpStatus.BAD_REQUEST, "해당 상품 리스트가 없습니다");
+    IS_EMPTY_LIST(HttpStatus.BAD_REQUEST, "해당 상품 리스트가 없습니다"),
+    IS_NULL(HttpStatus.BAD_REQUEST, "해당 상품이 없습니다");
 
     private final HttpStatus httpStatus;
     private final String message;
