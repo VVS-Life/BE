15466196ec1c6d27cac16d3acd93dd7be6235a73
HyeGwang Kang <kingaser@naver.com>
2023-10-17 21:18:50 +0900

    feat : 관리자 회원가입 추가, 반환값 ResponseEntity로 변경

--- src/main/java/com/example/vvs/domain/member/controller/MemberController.java
+++ src/main/java/com/example/vvs/domain/member/controller/MemberController.java
@@ -5,6 +5,7 @@ import com.example.vvs.domain.member.dto.JoinRequestDTO;
 import com.example.vvs.domain.member.dto.LoginRequestDTO;
 import com.example.vvs.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,19 +19,24 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @PostMapping("/join")
-    public MessageDTO postMember(@Valid @RequestBody JoinRequestDTO joinRequestDTO) {
+    @PostMapping("/join/member")
+    public ResponseEntity<MessageDTO> postMember(@Valid @RequestBody JoinRequestDTO joinRequestDTO) {
         return memberService.createMember(joinRequestDTO);
     }
 
+    @PostMapping("/join/admin")
+    public ResponseEntity<MessageDTO> postAdmin(@Valid @RequestBody JoinRequestDTO joinRequestDTO) {
+        return memberService.createAdmin(joinRequestDTO);
+    }
+
     @PostMapping("/login/member")
-    public MessageDTO postLogin(@RequestBody LoginRequestDTO loginRequestDTO,
+    public ResponseEntity<MessageDTO> postLogin(@RequestBody LoginRequestDTO loginRequestDTO,
                                 HttpServletResponse response) {
         return memberService.login(loginRequestDTO, response);
     }
 
     @PostMapping("/login/admin")
-    public MessageDTO postAdminLogin(@RequestBody LoginRequestDTO loginRequestDTO,
+    public ResponseEntity<MessageDTO> postAdminLogin(@RequestBody LoginRequestDTO loginRequestDTO,
                                      HttpServletResponse response) {
         return memberService.adminLogin(loginRequestDTO, response);
     }
