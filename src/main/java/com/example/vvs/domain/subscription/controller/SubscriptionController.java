6852f7c24b4c8f508d94f4cdacbda660f9ae096d
HyeGwang Kang <kingaser@naver.com>
2023-10-17 17:14:10 +0900

    feat : Subscription에 Security 적용, 조회, 수정로직 추가 및 예외 처리 종류 추가

--- src/main/java/com/example/vvs/domain/subscription/controller/SubscriptionController.java
+++ src/main/java/com/example/vvs/domain/subscription/controller/SubscriptionController.java
@@ -1,16 +1,16 @@
 package com.example.vvs.domain.subscription.controller;
 
 import com.example.vvs.domain.common.MessageDTO;
-import com.example.vvs.domain.member.entity.Member;
 import com.example.vvs.domain.subscription.dto.SubscriptionRequestDTO;
 import com.example.vvs.domain.subscription.dto.SubscriptionResponseDTO;
 import com.example.vvs.domain.subscription.service.SubscriptionService;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,17 +19,33 @@ public class SubscriptionController {
     private final SubscriptionService subscriptionService;
 
     @PostMapping("/subscription")
-    public ResponseEntity<SubscriptionResponseDTO> postSubscription(@RequestBody SubscriptionRequestDTO subscriptionRequestDTO) {
-        return subscriptionService.createSubscription(subscriptionRequestDTO);
+    public ResponseEntity<SubscriptionResponseDTO> postSubscription(@RequestBody SubscriptionRequestDTO subscriptionRequestDTO,
+                                                                    @AuthenticationPrincipal MemberDetailsImpl memberDetails) {
+        return subscriptionService.createSubscription(subscriptionRequestDTO, memberDetails.getMember());
     }
 
-    @GetMapping("/subscription/{member-id}")
-    public List<SubscriptionResponseDTO> getSubscriptionList(@PathVariable("member-id") Long memberId) {
-        return subscriptionService.findAllSubscription(memberId);
+    @GetMapping("/subscription/{subscription-id}")
+    public SubscriptionResponseDTO getSubscription(@AuthenticationPrincipal MemberDetailsImpl memberDetails,
+                                                   @PathVariable("subscription-id") Long subscriptionId) {
+        return subscriptionService.findSubscription(memberDetails.getMember(), subscriptionId);
+    }
+
+    @GetMapping("/subscription")
+    public Page<SubscriptionResponseDTO> getSubscriptionPage(@AuthenticationPrincipal MemberDetailsImpl memberDetails,
+                                                             @PageableDefault Pageable pageable) {
+        return subscriptionService.findAllSubscriptionPage(memberDetails.getMember().getId(), pageable);
+    }
+
+    @GetMapping("/subscription/admin")
+    public Page<SubscriptionResponseDTO> getAdminSubscriptionPage(@AuthenticationPrincipal MemberDetailsImpl memberDetails,
+                                                                  @PageableDefault Pageable pageable) {
+        return subscriptionService.findAllSubscriptionAdminPage(memberDetails.getMember(), pageable);
     }
 
     @PatchMapping("/subscription/admin/{subscription-id}")
-    public MessageDTO updateSubscription(@PathVariable("subscription-id") Long id, @RequestBody SubscriptionRequestDTO subscriptionRequestDTO) {
-        return subscriptionService.updateSubscription(id, subscriptionRequestDTO);
+    public MessageDTO updateSubscription(@PathVariable("subscription-id") Long id,
+                                         @RequestBody SubscriptionRequestDTO subscriptionRequestDTO,
+                                         @AuthenticationPrincipal MemberDetailsImpl memberDetails) {
+        return subscriptionService.updateSubscription(id, subscriptionRequestDTO, memberDetails.getMember());
     }
 }
--- src/main/java/com/example/vvs/domain/subscription/repository/SubscriptionRepository.java
+++ src/main/java/com/example/vvs/domain/subscription/repository/SubscriptionRepository.java
@@ -1,12 +1,13 @@
 package com.example.vvs.domain.subscription.repository;
 
 import com.example.vvs.domain.subscription.entity.Subscription;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
 
-    List<Subscription> findByMemberId(Long memberId);
+    Page<Subscription> findAllByOrderByIdDesc(Pageable pageable);
 
+    Page<Subscription> findAllByIdOrderByIdDesc(Long id, Pageable pageable);
 }
